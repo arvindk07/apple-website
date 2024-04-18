b902ead1db5d1b4aab52faa572967c11c1484ce3
arvindk07 <info.arvind07@gmail.com>
2024-04-19 01:22:39 +0530

    add assets & constants

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
-  );
+  return <main className="bg-black"></main>;
 };
 
 export default App;

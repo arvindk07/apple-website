cc94d9849a8752939fdb1e9a66cd9b84992d3fc9
arvindk07 <info.arvind07@gmail.com>
2024-04-24 01:40:54 +0530

    modal components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlight from "./components/Highlight";
+import Modal from "./components/Modal";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Highlight />
+      <Modal />
     </main>
   );
 };
